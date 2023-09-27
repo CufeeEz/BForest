de184a0c98695eccd51d6c54dbf89bee91a4e1b3
Daniil <92633278+CufeeEz@users.noreply.github.com>
2023-09-27 15:07:02 +0300

    up

--- src/main/java/my/cufee/bforest/Game/StartGame.java
+++ src/main/java/my/cufee/bforest/Game/StartGame.java
@@ -1,7 +1,16 @@
 package my.cufee.bforest.Game;
 
+import my.cufee.bforest.Arena.ArenaLocation;
 import my.cufee.bforest.Util.ChatUtil;
 import my.cufee.bforest.Util.PlayersCount;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.List;
 import java.util.Objects;
@@ -10,21 +19,46 @@ public class StartGame {
 
     static String murderRole = GetRole.GetMurderRole(PlayersCount.playersOnGame);
     public static void beginGame(List<String> ArrayPlayers){
-        preBeginGame(ArrayPlayers);
-
-
-
-
-
-
-    }
-    private static void preBeginGame(List<String> ArrayPlayers) {
         ChatUtil.GameRule(murderRole, PlayersCount.playersOnGame);
         for (int i = 0; i < ArrayPlayers.size(); i++) {
             if (Objects.equals(murderRole, ArrayPlayers.get(i))) {
-                PlayersCount.SurvivorPlayers.add(ArrayPlayers.get(i));
+
+                Player MurderPlayer = Bukkit.getPlayerExact(ArrayPlayers.get(i));
+                MurderPlayer.teleport(ArenaLocation.getLocSpawnMurder());
+                // Отчистка игрока и подготовка его к игре
+                MurderPlayer.setHealth(20);
+                MurderPlayer.getInventory().clear();
+                MurderPlayer.setGameMode(GameMode.ADVENTURE);
+                MurderPlayer.setFoodLevel(20);
+                MurderPlayer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 9999, 1));
+                MurderPlayer.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 9999, 1));
+                MurderPlayer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 9999, 2));
+                ItemStack Sword = new ItemStack(Material.STONE_SWORD);
+                Sword.addEnchantment(Enchantment.DAMAGE_ALL, 5);
+                MurderPlayer.getInventory().addItem(Sword);
+            }
+            else{
+                Player SurvivorPlayer = Bukkit.getPlayerExact(ArrayPlayers.get(i));
+                SurvivorPlayer.teleport(ArenaLocation.getLocSpawnSurvivor());
+                // Отчистка игрока и подготовка его к игре
+                SurvivorPlayer.setHealth(20);
+                SurvivorPlayer.getInventory().clear();
+                SurvivorPlayer.setGameMode(GameMode.ADVENTURE);
+                SurvivorPlayer.setFoodLevel(20);
+
+                SurvivorPlayer.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 9999, 1));
+                ItemStack Stick = new ItemStack(Material.STICK);
+                Stick.addUnsafeEnchantment(Enchantment.KNOCKBACK, 3);
+                SurvivorPlayer.getInventory().addItem(Stick);
+
             }
         }
-    }
 
+
+
+
+
+
+
+    }
 }
